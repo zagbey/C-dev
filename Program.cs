4354fbb3fd44bf8e30941a3f9fdb3b8cfaae8e94
zagbey <49595767+zagbey@users.noreply.github.com>
2022-07-27 20:51:11 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int deger = 2;
+string degisken=null;
+string Degisken = null;
+string zag_gncr = null;
+byte b = 5;  // 1 byte yer kaplar
+sbyte c = 5;
+
+short s = 5; 
+ushort us = 5; //22 byte yer 
+
+Int16 i16 = 2;  //2 byte yer kaplar
+int i = 2 ;     // 4 byte 
+Int32 i32 = 2;  // 4 byte 
+Int64 zc = 2;      //8byte 
+
+uint ui = 2;
+long l  = 4;
+ulong ul = 5; 
+
+float f = 5;
+double d = 5 ;
+decimal de = 5 ;
+
+char ch = '2';
+string str = "zeynel";
+
+bool b1 = true;
+bool b2 = false;
+
+DateTime dt = DateTime.Now;
+Console.WriteLine(dt);
+
+//string ifadeler 
+
+string zag = string.Empty;
+zag = "zeynel abidin";
+string str20 = "20";
+int int20 = 20;
+string yeniDeger = str20 + int20.ToString();
+Console.WriteLine(yeniDeger);
+
+
+//integar tanımlama
+
+int integar1 = 5;
+int integer2 = 4;
+int integar3 = integar1*integer2;
+
+// DATE TİME
+string datetime2 = DateTime.Now.ToString("dd.MM.yyyy");
+Console.WriteLine(datetime2);
